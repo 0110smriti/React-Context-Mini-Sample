38e61bd7a0d9d6f2cb565703ba1050aebbc7c187
Smriti <chaturvedi.smriti1@gmail.com>
2024-08-13 11:30:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Login } from './components/Login/Login'
+import Profile from './components/Profile/Profile'
+import UserContextProvider from './context/UserContextProvider'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>React Context</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useContext } from 'react'
+import UserContext from '../../context/UserContext'
+
+export const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        // Prevent page from refresh
+        e.preventDefault()
+        setUser({username, password})
+    }
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder='Username' />
+        {" "}
+        <input type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
